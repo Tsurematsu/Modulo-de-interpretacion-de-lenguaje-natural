81ded79de808519a44142313c5b37b57efa9b909
Tsurematsu <daniel120802@gmail.com>
2024-06-26 16:20:12 -0500

    saved

--- public/utils/statistics.js
+++ public/utils/statistics.js
@@ -2,7 +2,7 @@ export default new function statistics() {
     this.void = (ctx)=>{
         this.ctx = ctx;
     }
-    this.loop = (data)=>{
+    this.loop = ({hands, pose, endTime})=>{
         // console.log(data);
     }
 }
